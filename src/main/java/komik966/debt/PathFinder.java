4bcb55601c300f7d21875b51375a9a114a28198e
konradmikucki <komik966@gmail.com>
2018-05-10 08:39:56 +0200

    path finder in progress

--- src/main/java/komik966/debt/PathFinder.java
+++ src/main/java/komik966/debt/PathFinder.java
@@ -4,30 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class PathFinder {
-//    List<Person> findPath(
-//            Map<Person, Map<Person, Integer>> borrowersGraph,
-//            Person from,
-//            Person to
-//    ) throws BorrowerNotFound {
-//        if (!borrowersGraph.containsKey(from)) {
-//            throw new BorrowerNotFound();
-//        }
-//
-//        List<Person> path = new ArrayList<>();
-//        path.add(from);
-//
-//        borrowersGraph.get(from).keySet().forEach(lender -> {
-//            try {
-//                path.addAll(findPath(borrowersGraph, lender, to));
-//            } catch (BorrowerNotFound borrowerNotFound) {
-//            }
-//        });
-//
-//
-//        return path;
-//    }
-//
-//    private class BorrowerNotFound extends Throwable {
-//    }
+class PathFinder {
+    List<Person> findPath(
+            BorrowersGraph borrowersGraph,
+            Person src,
+            Person dest
+    ) throws BorrowerNotFound {
+        if (!borrowersGraph.containsKey(from)) {
+            throw new BorrowerNotFound();
+        }
+
+        List<Person> path = new ArrayList<>();
+        path.add(from);
+
+        borrowersGraph.get(from).keySet().forEach(lender -> {
+            try {
+                path.addAll(findPath(borrowersGraph, lender, to));
+            } catch (BorrowerNotFound borrowerNotFound) {
+            }
+        });
+
+
+        return path;
+    }
+
+    class BorrowerNotFound extends Throwable {
+    }
 }
--- src/test/java/komik966/debt/PathFinderTest.java
+++ src/test/java/komik966/debt/PathFinderTest.java
@@ -1,14 +1,55 @@
 package komik966.debt;
 
+import com.google.inject.Guice;
+import komik966.debt.guice.DebtModule;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 
 public class PathFinderTest {
+    private PathFinder pathFinder;
+    private BorrowersGraph borrowersGraph;
+    private Person pA;
+    private Person pB;
+    private Person pC;
+    private Person pD;
+    private Person pE;
+    private Person pF;
+
+    @Before
+    public void setUp() {
+        pathFinder = new PathFinder();
+        PersonFactory personFactory = Guice.createInjector(new DebtModule()).getInstance(PersonFactory.class);
+        /*
+         * +---+   60  +---+   50 +---+   40  +---+
+         * | F | <---- | E | <--- | A | <---- | D |
+         * +---+       +---+      +---+       +---+
+         *                          | 10        ^
+         *                          V           | 30
+         *                        +---+  20   +---+
+         *                        | B | ----> | C |
+         *                        +---+       +---+
+         */
+        pA = personFactory.create("A");
+        pB = personFactory.create("B");
+        pC = personFactory.create("C");
+        pD = personFactory.create("D");
+        pE = personFactory.create("E");
+        pF = personFactory.create("F");
+        borrowersGraph.increaseEdgeValue(pA, pB, 40);
+        borrowersGraph.increaseEdgeValue(pB, pC, 20);
+        borrowersGraph.increaseEdgeValue(pC, pD, 30);
+        borrowersGraph.increaseEdgeValue(pD, pA, 40);
+        borrowersGraph.increaseEdgeValue(pA, pE, 50);
+        borrowersGraph.increaseEdgeValue(pE, pF, 60);
+    }
 
     @Test
-    public void findPath() {
-//        PathFinder pF = new PathFinder();
-//        borrowersGrap
+    public void findPath() throws PathFinder.BorrowerNotFound {
+        List<Person> foundPath = pathFinder.findPath(borrowersGraph, pA, pB);
+//        assertThat(foundPath).contains
     }
 }
